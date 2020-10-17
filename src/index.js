39d939bf3f08d4cf3c304106d216189453e4c83b
onc157 <onc157@yandex.ru>
2020-10-17 23:27:31 +0300

    feat: solved the brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const ARRAY_BRACKETS_CONFIG = [];
+  bracketsConfig.forEach(item => (ARRAY_BRACKETS_CONFIG.push(item[0].concat(item[1]))));
+
+  let getPairOfBrackets;
+  let stringToCut = str;
+
+  for (let i = 0; i < ARRAY_BRACKETS_CONFIG.length; i = i + 1) {
+    getPairOfBrackets = ARRAY_BRACKETS_CONFIG[i];
+    stringToCut = stringToCut.replace(getPairOfBrackets, '');
+  }
+  if (stringToCut === '') {
+    return true;
+  } else if (stringToCut === str) {
+    return false;
+  } else return check(stringToCut, bracketsConfig);
 }
